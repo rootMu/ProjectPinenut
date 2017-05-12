1afd0fac4a1011cce90a4268b5ced3b2796391cb
Matthew Howells <matthew.howells@bglgroup.co.uk>
2017-05-12 17:29:47 +0100

    First commit

---
+++ app/src/main/java/com/projects/rootmu/projectpinenut/Item.java
@@ -0,0 +1,21 @@
+package com.projects.rootmu.projectpinenut;
+
+
+public class Item {
+
+    private String title;
+
+    public Item() {}
+
+    public Item(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
